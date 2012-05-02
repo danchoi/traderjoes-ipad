d52fc9e99f9ef52dc43109bbee670016be19be3d
Daniel Choi <dhchoi@gmail.com>
2012-05-02 11:52:49 -0400

    Let's make an item

--- traderjoes_ipad/MasterViewController.m
+++ traderjoes_ipad/MasterViewController.m
@@ -160,7 +160,7 @@
   
   NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
   // Edit the entity name as appropriate.
-  NSEntityDescription *entity = [NSEntityDescription entityForName:@"Event" inManagedObjectContext:self.managedObjectContext];
+  NSEntityDescription *entity = [NSEntityDescription entityForName:@"Item" inManagedObjectContext:self.managedObjectContext];
   [fetchRequest setEntity:entity];
   
   // Set the batch size to a suitable number.
